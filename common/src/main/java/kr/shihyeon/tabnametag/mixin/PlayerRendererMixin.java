bcc767a0c3645ec404d21cde0a497dde3f96b0fe
Seungyeop Lee <lsy020206@outlook.kr>
2025-05-08 20:26:12 +0900

    update 1.21.2

--- common/src/main/java/kr/shihyeon/tabnametag/mixin/PlayerRendererMixin.java
+++ common/src/main/java/kr/shihyeon/tabnametag/mixin/PlayerRendererMixin.java
@@ -2,9 +2,9 @@ package kr.shihyeon.tabnametag.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.renderer.entity.state.PlayerRenderState;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,18 +14,20 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public abstract class PlayerRendererMixin {
 
     @ModifyVariable(
-            method = "renderNameTag(Lnet/minecraft/client/player/AbstractClientPlayer;Lnet/minecraft/network/chat/Component;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IF)V",
+            method = "renderNameTag(Lnet/minecraft/client/renderer/entity/state/PlayerRenderState;Lnet/minecraft/network/chat/Component;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
             at = @At("HEAD"),
             argsOnly = true
     )
-    private Component tabnametag$renderNameTag(Component name, AbstractClientPlayer abstractClientPlayer, Component originalName, PoseStack poseStack, MultiBufferSource multiBufferSource, int i) {
+    private Component tabnametag$renderNameTag(Component name, PlayerRenderState playerRenderState, Component originalName, PoseStack poseStack, MultiBufferSource multiBufferSource, int i) {
         Minecraft client = Minecraft.getInstance();
         if (client.player == null) return name;
 
         return client.player.connection.getOnlinePlayers().stream()
-                .filter(p -> p.getProfile().getId().equals(abstractClientPlayer.getUUID()))
+                .filter(p -> p.getProfile().getName().equals(playerRenderState.name))
                 .findFirst()
-                .map(p -> p.getTabListDisplayName() != null ? p.getTabListDisplayName() : Component.literal(p.getProfile().getName()))
-                .orElse(abstractClientPlayer.getDisplayName());
+                .map(p -> p.getTabListDisplayName() != null
+                        ? p.getTabListDisplayName()
+                        : Component.literal(p.getProfile().getName()))
+                .orElse(name);
     }
 }
